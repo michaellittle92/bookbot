65619357f3cab7e4a84e698e8f35163c2e7e2f44
Michael Little <michaellittle92@gmail.com>
2025-05-10 14:30:52 +1000

    rewrote get_num_char to be a list of dictionaries sorted in reverse order

--- stats.py
+++ stats.py
@@ -10,7 +10,7 @@ def get_num_words(file_path):
     word_count = len(list_of_words)
     print(f"{word_count} words found in the document")
 
-def sort_on():
+def sort_on(dict):
     return dict["num"]
 
 
@@ -18,11 +18,15 @@ def sort_on():
 def get_num_char(file_path):
     file_contents = get_book_text(file_path).lower()
     list_of_chars = list(file_contents)
-    letter_count = {}
+    letter_count = []
     for letter in list_of_chars:
-        if letter in letter_count:
-            letter_count[letter] += 1
-        else:
-            letter_count[letter] = 1
-   
-    print(letter_count)
+        if not any(d["char"] == letter for d in letter_count):
+            char_dict = {"char": letter, "num": 1}
+            letter_count.append(char_dict)
+        else: 
+            for entry in letter_count:
+                if letter == entry["char"]:
+                    entry["num"] += 1 
+    letter_count.sort(reverse=True, key=sort_on)
+    return(letter_count)
+
